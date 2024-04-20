3bacd3d8dcb60a7ae682666a1854372b6bb9a133
Alexsander Valente <alevtelles@gmail.com>
2024-04-20 11:42:56 -0300

    update cadastro medico

--- medico/views.py
+++ medico/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from .models import Especialidades
+from django.http import HttpResponse
 
 # Cadastro de médico
-
 def cadastro_medico(request):
   if request.method == "GET":
-    return render(request, 'cadastro_medico.html')
+    especialidades = Especialidades.objects.all()
+    return render(request, 'cadastro_medico.html', {'especialidades': especialidades})
+  elif request.method == "POST":
+    crm = request.POST.get('crm')
+    nome = request.POST.get('nome')
+    cep = request.POST.get('cep')
+    rua = request.POST.get('rua')
+    bairro = request.POST.get('bairro')
+    numero = request.POST.get('numero')
+    cim = request.FILES.get('cim')
+    rg = request.FILES.get('rg')
+    foto = request.FILES.get('foto')
+    especialidade = request.POST.get('especialidade')
+    descricao = request.POST.get('descricao')
+    valor_consulta = request.POST.get('valor_consulta')
+
+    return HttpResponse(f'{especialidade} - {cim}')
+  
