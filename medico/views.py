818eb4be0a4530db53a7a49eee9e5cdb807a7b0a
Alexsander Valente <alevtelles@gmail.com>
2024-05-01 12:02:01 -0300

    feat: create view medico

--- medico/views.py
+++ medico/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Especialidades
+from django.shortcuts import render, redirect
+from .models import Especialidades, DadosMedico
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Cadastro de médico
 def cadastro_medico(request):
@@ -21,5 +23,24 @@ def cadastro_medico(request):
     descricao = request.POST.get('descricao')
     valor_consulta = request.POST.get('valor_consulta')
 
-    return HttpResponse(f'{especialidade} - {cim}')
+    dados_medico = DadosMedico(
+      crm=crm,
+      nome=nome, 
+      cep=cep,
+      rua=rua,
+      bairro=bairro,
+      numero=numero,
+      cedula_identidade_medica=cim,
+      rg=rg,
+      foto=foto,
+      especialidade_id=especialidade,
+      descricao=descricao,
+      valor_consulta=valor_consulta,
+      user=request.user
+      
+    )
   
+    dados_medico.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Cadastro médico realizado com sucesso!')
+    return redirect('/medicos/abrir_horario')
